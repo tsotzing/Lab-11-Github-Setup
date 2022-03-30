374a230f14e5252e1cbdb4630b52ccaaec250a61
dcd11 <dcduggins115@gmail.com>
2022-03-30 09:52:03 -0400

    3/30/22

--- EMain.java
+++ EMain.java
@@ -11,50 +11,53 @@ public class EMain {
     private JButton encrypt;
     private JButton decrypt;
 
-    public static void main(String[] args) {
-        // create and set up the window.
-        frame = new JFrame("Encrypt Cipher");
-        fieldsPanel = new JPanel();
-        buttonPane = new JPanel();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fieldsPanel.setLayout(new BoxLayout(fieldsPanel, BoxLayout.PAGE_AXIS));
-        buttonPane.setLayout(new FlowLayout());
+    public EMain {
+        public static void main (String[]args){
+            // create and set up the window.
+            frame = new JFrame("Encrypt Cipher");
+            fieldsPanel = new JPanel();
+            buttonPane = new JPanel();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            fieldsPanel.setLayout(new BoxLayout(fieldsPanel, BoxLayout.PAGE_AXIS));
+            buttonPane.setLayout(new FlowLayout());
 
-        // label setup
-        label = new JLabel("Filename");
-        frame.getContentPane().add(label);
-        JTextField filename = new JTextField();
-        filename.setPreferredSize(new Dimension(250, 40));
+            // label setup
+            label = new JLabel("Filename");
+            frame.getContentPane().add(label);
+            JTextField filename = new JTextField();
+            filename.setPreferredSize(new Dimension(250, 40));
 
-        label2 = new JLabel("Key");
-        frame.getContentPane().add(label2);
-        JTextField key = new JTextField();
-        key.setPreferredSize(new Dimension(250, 40));
+            label2 = new JLabel("Key");
+            frame.getContentPane().add(label2);
+            JTextField key = new JTextField();
+            key.setPreferredSize(new Dimension(250, 40));
 
-        // button setup
-        encrypt = new JButton("Encrypt");
-        encrypt.addActionListener(new ButtonListener());
-        frame.getContentPane().add(encrypt);
+            // button setup
+            encrypt = new JButton("Encrypt");
+            encrypt.addActionListener(new ButtonListener());
+            frame.getContentPane().add(encrypt);
 
-        decrypt = new JButton("Decrypt");
-        decrypt.addActionListener(new ButtonListener());
-        frame.getContentPane().add(decrypt);
+            decrypt = new JButton("Decrypt");
+            decrypt.addActionListener(new ButtonListener());
+            frame.getContentPane().add(decrypt);
 
 
-        fieldsPanel.add(label);
-        fieldsPanel.add(filename);
-        fieldsPanel.add(label2);
-        fieldsPanel.add(key);
-        buttonPane.add(encrypt);
-        buttonPane.add(decrypt);
-        frame.add(fieldsPanel, BorderLayout.PAGE_START);
-        frame.add(buttonPane, BorderLayout.PAGE_END);
-        frame.pack();
-        frame.setVisible(true);    }
+            fieldsPanel.add(label);
+            fieldsPanel.add(filename);
+            fieldsPanel.add(label2);
+            fieldsPanel.add(key);
+            buttonPane.add(encrypt);
+            buttonPane.add(decrypt);
+            frame.add(fieldsPanel, BorderLayout.PAGE_START);
+            frame.add(buttonPane, BorderLayout.PAGE_END);
+            frame.pack();
+            frame.setVisible(true);
+        }
 
-    static class ButtonListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
+        static class ButtonListener implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
         }
     }
 }
