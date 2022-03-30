11b8b5cea0afc1ada615cb1d20852ae502dc7906
dcd11 <dcduggins115@gmail.com>
2022-03-30 09:59:57 -0400

    3/30/22

---
+++ Main.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+
+        public static void main (String[]args){
+            // create and set up the window.
+            JFrame frame = new JFrame("Encrypt Cipher");
+            JPanel fieldsPanel = new JPanel();
+            JPanel buttonPane = new JPanel();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            fieldsPanel.setLayout(new BoxLayout(fieldsPanel, BoxLayout.PAGE_AXIS));
+            buttonPane.setLayout(new FlowLayout());
+
+            // label setup
+            JLabel label = new JLabel("Filename");
+            frame.getContentPane().add(label);
+            JTextField filename = new JTextField();
+            filename.setPreferredSize(new Dimension(250, 40));
+
+            JLabel label2 = new JLabel("Key");
+            frame.getContentPane().add(label2);
+            JTextField key = new JTextField();
+            key.setPreferredSize(new Dimension(250, 40));
+
+            // button setup
+            JButton encrypt = new JButton("Encrypt");
+            encrypt.addActionListener(new ButtonListener());
+            frame.getContentPane().add(encrypt);
+
+            JButton decrypt = new JButton("Decrypt");
+            decrypt.addActionListener(new ButtonListener());
+            frame.getContentPane().add(decrypt);
+
+
+            fieldsPanel.add(label);
+            fieldsPanel.add(filename);
+            fieldsPanel.add(label2);
+            fieldsPanel.add(key);
+            buttonPane.add(encrypt);
+            buttonPane.add(decrypt);
+            frame.add(fieldsPanel, BorderLayout.PAGE_START);
+            frame.add(buttonPane, BorderLayout.PAGE_END);
+            frame.pack();
+            frame.setVisible(true);
+        }
+
+        static class ButtonListener implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        }
+    }
+
+
